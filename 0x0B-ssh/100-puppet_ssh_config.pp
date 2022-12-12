d5e59472f197efd82c17fda6375402caad6d2d07
brianmugunga <104896959+brianmugunga@users.noreply.github.com>
2022-12-12 17:15:49 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
-# SSH client configuration
-exec { 'echo -e "IdentityFile ~/.ssh/holberton\n" >> /etc/ssh/ssh_config':
-  provider => shell,
-  path    => '/etc/ssh/ssh_config',
-  command => 'echo -e "PasswordAuthentication no\n" >> /etc/ssh/ssh_config'
+# Config file using puppet
+
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+  path => '/bin/'
 }
